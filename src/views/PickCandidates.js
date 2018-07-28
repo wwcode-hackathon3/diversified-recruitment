b7ca0770cf020e136f48cf32a6b3993888c8962a
Peining Pan <>
2018-07-28 18:16:02 +0800

    load candidates from json

--- src/views/PickCandidates.js
+++ src/views/PickCandidates.js
@@ -1,25 +1,6 @@
 import React from "react";
 import WordCloud from 'react-d3-cloud';
-import JsonData from '../output.json';
-
-const human_1 = [
-  { text: 'Apple', value: 1000 },
-  { text: 'Banana', value: 200 },
-  { text: 'Tomato', value: 800 },
-  { text: 'Cat', value: 1000000 },
-  { text: 'Very Interesting', value: 10 },
-];
-
-
-const human_2 = [
-  { text: 'Hey', value: 1000 },
-  { text: 'lol', value: 200 },
-  { text: 'first impression', value: 800 },
-  { text: 'very cool', value: 1000000 },
-  { text: 'duck', value: 10 },
-];
-
-const resumes = [human_1, human_2];
+import candidates from '../candidates.json';
 
 
 const fontSizeMapper = word => Math.log2(word.value) * 5;
@@ -34,22 +15,6 @@ class AddJobDescription extends React.Component {
     this.dislike = this.dislike.bind(this);
     this.like = this.like.bind(this);
     this.moveToNext = this.moveToNext.bind(this);
-    this.processJson = function() {
-        //
-        // console.log(JsonData.highlight[0]);
-        // console.log(JsonData.highlight);
-        for (let i = 0; i <10; i++) {
-          console.log(i);
-          let candidate = JsonData.highlight[i];
-          for (let j = 0; j < candidate.length; j++) {
-            let attribute = candidate[j];
-            if (attribute.category === "Skills") {
-              console.log(attribute);
-            }
-          }
-      }
-    }
-    this.processJson();
   }
 
   moveToNext() {
@@ -68,11 +33,15 @@ class AddJobDescription extends React.Component {
     let word_cloud;
     let like_button;
     let dislike_button;
-    if (this.state.index < resumes.length) {
-      word_cloud = <WordCloud data={resumes[this.state.index]} fontSizeMapper={fontSizeMapper} />;
+    let name;
+    if (this.state.index < candidates.length) {
+      let word_cloud_data = candidates[this.state.index].skills;
+      name = <p>{candidates[this.state.index].name}</p>;
+      word_cloud = <WordCloud data={word_cloud_data} fontSizeMapper={fontSizeMapper} />;
       like_button = <button onClick={this.like} className="btn btn-success">I like you</button>;
       dislike_button = <button onClick={this.dislike} className="btn btn-danger"> I no lik u </button>
     } else {
+      name = <br />;
       word_cloud = <p>The End</p>;
       like_button = <br />;
       dislike_button = <br />;
@@ -88,6 +57,7 @@ class AddJobDescription extends React.Component {
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 className="h2">Pick candidates</h1>
       </div>
+      {name}
       {word_cloud}
       <div className="text-center">
         {like_button}
