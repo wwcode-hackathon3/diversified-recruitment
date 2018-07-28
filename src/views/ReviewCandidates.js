a7c677a4edf54d6173e17b986d80a61f4c5fa42a
chaitanyarb <40911484+chaitanyarb@users.noreply.github.com>
2018-07-28 18:18:41 +0800

    nav bar in review

--- src/views/ReviewCandidates.js
+++ src/views/ReviewCandidates.js
@@ -23,6 +23,7 @@ export default class AddJobDescription extends React.Component {
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="/">Home</a></li>
                     <li class="breadcrumb-item active" aria-current="page">Review Candidates</li>
+                </ol>
             </nav>
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 className="h2">Review candidates</h1>
