9ec263422bb123e2a55d37b02cd678cb2ddc2262
Peining Pan <>
2018-07-28 14:36:37 +0800

    Add word cloud

--- src/views/PickCandidates.js
+++ src/views/PickCandidates.js
@@ -1,9 +1,26 @@
 import React from "react";
+import WordCloud from 'react-d3-cloud';
+
+const data = [
+  { text: 'Hey', value: 1000 },
+  { text: 'lol', value: 200 },
+  { text: 'first impression', value: 800 },
+  { text: 'very cool', value: 1000000 },
+  { text: 'duck', value: 10 },
+];
+
+const fontSizeMapper = word => Math.log2(word.value) * 5;
+
 
 export default class AddJobDescription extends React.Component {
   render() {
     return <div>
       <h1>Pick candidates</h1>
+      <WordCloud
+        data={data}
+        fontSizeMapper={fontSizeMapper}
+        // rotate={rotate}
+      />,
     </div>
   }
 }
