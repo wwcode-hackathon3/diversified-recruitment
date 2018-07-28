72054201d87a5e5c27f4f90ef9702a66344f0657
Sue Zheng Hao <szhlibrary@gmail.com>
2018-07-28 13:23:58 +0800

    UI: Blatantly copied code from bootstrap example

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <button type="button" className="btn btn-primary">Primary</button>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
+            <ul className="navbar-nav px-3">
+                <li className="nav-item text-nowrap">
+                    <a className="nav-link" href="#">Sign out</a>
+                </li>
+            </ul>
+        </nav>
+        <div className="container-fluid">
+          <div className="row">
+            <nav className="col-md-2 d-none d-md-block bg-light sidebar">
+              <div className="sidebar-sticky">
+                <ul className="nav flex-column">
+                  <li className="nav-item">
+                    <a className="nav-link active" href="#">
+                      Add job description <span className="sr-only">(current)</span>
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Pick candidates
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Review candidates
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+
+            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+              <div
+                className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 className="h2">Add job description</h1>
+              </div>
+            </main>
+          </div>
+        </div>
       </div>
     );
   }
