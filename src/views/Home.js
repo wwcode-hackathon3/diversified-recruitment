1eebc60ec473470b5943aa7b2a994356f8a4d640
heyjiawei <jaewei.j.w@gmail.com>
2018-07-28 15:16:13 +0800

    fix number of applicants pill

--- src/views/Home.js
+++ src/views/Home.js
@@ -14,14 +14,18 @@ export default class Home extends React.Component {
         <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
           <div className="d-flex w-100 justify-content-between">
             <h5 className="mb-1">Senior Java Developer</h5>
-            <span className="badge badge-primary badge-pill">14</span>
+            <div>
+              <span className="badge badge-primary badge-pill">14</span>
+            </div>
           </div>
           <small>Last applicant applied 5 hours ago</small>
         </a>
         <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
           <div className="d-flex w-100 justify-content-between">
             <h5 className="mb-1">Junior ReactJS Developer</h5>
-            <span className="badge badge-primary badge-pill">5</span>
+            <div>
+              <span className="badge badge-primary badge-pill">5</span>
+            </div>
           </div>
           <small className="text-muted">Last applicant applied 3 days ago</small>
         </a>
