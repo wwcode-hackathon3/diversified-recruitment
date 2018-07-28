cf00b30a26f6174d57853c364fbcb217157e7deb
Sue Zheng Hao <szhlibrary@gmail.com>
2018-07-28 19:14:55 +0800

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, NavLink, Redirect} from "react-router-dom";
+import {BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 import './App.css';
 import Home from './views/Home'
 import AddJobDescription from './views/AddJobDescription'
