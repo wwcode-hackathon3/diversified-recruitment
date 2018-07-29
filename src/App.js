7fc8bc014af228f762080ba6d7536937ec23cd49
Sue Zheng Hao <szhlibrary@gmail.com>
2018-07-29 11:45:23 +0800

    Misc: Changed page title and linked logo to homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Redirect, Link} from "react-router-dom";
 import './App.css';
+import 'react-quill/dist/quill.snow.css';
 import Home from './views/Home'
 import AddJobDescription from './views/AddJobDescription'
 import PickCandidates from './views/PickCandidates'
@@ -13,7 +14,9 @@ class App extends Component {
       <Router>
         <div className="App">
           <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow rainbow">
-          <img src={logo} className="logo"/>
+            <Link className="navbar-brand col-sm-3 col-md-2 mr-0" to="/home">
+              <img src={logo} className="logo"/>
+            </Link>
             <ul className="navbar-nav px-3">
               <li className="nav-item text-nowrap">
                 <a className="nav-link" href="#">Sign out</a>
